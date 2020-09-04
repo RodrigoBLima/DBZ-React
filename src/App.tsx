fd110463fca663f176925fbf9373ed0d15a3747a
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-09-04 20:37:57 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import  GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles";
+import Routes from "./routes";
 
 function App() {
   return (
     <>
+      <Routes />
       <GlobalStyles />
     </>
   );
---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+
+// COMPONENTS
+
+import { Characters } from "./pages/Characters";
+import { Character } from "./pages/Character";
+import { Planets } from "./pages/Planets";
+import { Planet } from "./pages/Planet";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Characters} />
+      <Route path="/character/:id" component={Character} />
+      <Route path="/planets" component={Planets} />
+      <Route path="/planets/:id" component={Planet} />
+    </BrowserRouter>
+  );
+}
