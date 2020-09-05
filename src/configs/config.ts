8a94d535cdf721050ecab835d032edecb4b32271
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-09-04 21:10:32 -0300

    add new urls to config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,3 +1,5 @@
 export default {
     API_URL: "https://dragon-ball-api.herokuapp.com/api/",
+    PLANET_URL: "https://dragon-ball-api.herokuapp.com/api/planet/",
+    CHARACTER_URL: "https://dragon-ball-api.herokuapp.com/api/character/",
 }
